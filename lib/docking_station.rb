3c42f75bae8cd713ccd39dd0b4d63b0de6a0d31b
Callum Marshall <callum_marshall@hotmail.co.uk>
2019-04-04 16:29:25 +0100

    add dock method and bike attribute reader

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,15 @@
+require_relative 'bike'
+
 class DockingStation
+
   def release_bike
     bike = Bike.new
   end
+
+  def dock(bike)
+    @bike = bike
+  end
+
+  attr_reader :bike
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,20 @@ require 'docking_station'
 
 describe DockingStation do
   it {is_expected.to respond_to(:release_bike)}
+
+  it "releases working bike" do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
+  it "can you dock bike?" do
+    bike = subject.release_bike
+    expect(subject.dock(bike)).to eq bike
+  end
+
+
+  # it "bike exists in docking station" do
+  #    expect bike to exist in docking station
+  # end
+
 end
