253d48f4f0e180dd4549313d6c26b60c28c6c894
Callum Marshall <callum_marshall@hotmail.co.uk>
2019-04-05 14:45:46 +0100

    add docking station full error

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,17 +4,13 @@ class DockingStation
 
   attr_reader :bike
 
-  
-
   def release_bike
     fail 'No bikes available' unless @bike
     @bike
   end
 
   def dock(bike)
+    fail "docking station is full" if @bike
     @bike = bike
   end
-
-
-
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,7 +8,7 @@ describe DockingStation do
     #expect(bike).to be_working
   #end
 
-  describe '#release bike' do
+  describe '#release_bike' do
     it 'releases bike' do
       bike = Bike.new
       subject.dock(bike)
@@ -18,14 +18,19 @@ describe DockingStation do
       expect { subject.release_bike }.to raise_error('No bikes available')
     end
   end
+  describe '#dock' do
+    it "can you dock bike?" do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
 
-  it "can you dock bike?" do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+    it "raises error when trying to return bike to full dock" do
+      bike1 = Bike.new
+      bike2 = Bike.new
+      subject.dock(bike1)
+      expect {subject.dock(bike2)}.to raise_error("docking station is full")
+    end
   end
-
-
- 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
 end
