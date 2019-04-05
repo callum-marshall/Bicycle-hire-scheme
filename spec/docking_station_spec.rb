445be0546ba2f2d30983cf371143b189fe76d85e
MaxRobertsDear <maxhroberts@gmail.com>
2019-04-05 14:02:23 +0100

    add error message

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,21 +3,29 @@ require 'docking_station'
 describe DockingStation do
   it {is_expected.to respond_to(:release_bike)}
 
-  it "releases working bike" do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  #it "releases working bike" do
+    #bike = subject.release_bike
+    #expect(bike).to be_working
+  #end
+
+  describe '#release bike' do
+    it 'releases bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+    it "raises error when no bikes available" do
+      expect { subject.release_bike }.to raise_error('No bikes available')
+    end
   end
 
   it "can you dock bike?" do
-    bike = subject.release_bike
+    bike = Bike.new
     expect(subject.dock(bike)).to eq bike
   end
 
-  it "raises error when no bikes available" do
-    expect { subject.release_bike }.to raise_error
-  end
-  # it "bike exists in docking station" do
-  #    expect bike to exist in docking station
-  # end
+
+ 
+  it { is_expected.to respond_to(:dock).with(1).argument }
 
 end
